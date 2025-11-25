e9ba5d63a05580742e10018bd473c751a2804276
Imran_Shabbir <Imran.Shabbir@senew-tech.com>
2025-11-25 23:36:33 +0500

    api-adjusted

--- config/api.js
+++ config/api.js
@@ -4,5 +4,6 @@ const API_IMAGE_BASE_URL = 'https://api.convoytravels.pk';
 
 // const API_BASE_URL = 'http://localhost:5000/api/v1';
 // const API_IMAGE_BASE_URL = 'http://localhost:5000';
+// api adjusted 
 
 export { API_BASE_URL, API_IMAGE_BASE_URL };
