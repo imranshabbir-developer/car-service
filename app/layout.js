01d16e976dd440fb52063659c6004e2f91e0fd90
Imran_Shabbir <Imran.Shabbir@senew-tech.com>
2025-12-05 22:55:08 +0500

    head-tags

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import ConditionalLayout from "@/components/ConditionalLayout";
+import Script from "next/script";
 import "./globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -17,12 +18,35 @@ export const metadata = {
     index: true,
     follow: true,
   },
+  verification: {
+    google: "rnWeToOa_ED1J9NwEYe6omABuuJ4s2-dudf1JVJmM0M",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Search Console Verification */}
+        <meta
+          name="google-site-verification"
+          content="rnWeToOa_ED1J9NwEYe6omABuuJ4s2-dudf1JVJmM0M"
+        />
+      </head>
       <body suppressHydrationWarning>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0NZQWLFL7B"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0NZQWLFL7B');
+          `}
+        </Script>
         <ConditionalLayout>{children}</ConditionalLayout>
       </body>
     </html>
